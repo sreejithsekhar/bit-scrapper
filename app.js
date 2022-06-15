8e91e5088def0e0645bb0b73d4215496403ad6b5
sreejithsekhar <sekhar.sreejith@gmail.com>
2022-06-15 13:45:58 -0600

    Initial changes

---
+++ app.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer");
+const INDICATORS_CONSTANTS = require("./constants");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: {
+      width: 1000,
+      height: 480,
+    },
+    //slowMo: 250,
+  });
+
+  console.log('====INDICATORS_CONSTANTS===', INDICATORS_CONSTANTS);
+
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.binance.com/en/trade/BTC_USDT?theme=dark&type=spot"
+  );
+
+  await page.click("#onetrust-accept-btn-handler");
+  await page.waitForSelector("#Time");
+  await page.click("#Time");
+
+  const pageLooper = setInterval(async () => {
+    const indicators = await page.$$eval(
+      ".chart-title-indicator-container span",
+      (nodes) =>
+        nodes
+        .filter((n => n.innerText !== 'NaN'))
+        .reduce((acc, n) => {
+          acc[n.getAttribute("key")] = n.innerText;
+          return acc;
+        }, {})
+    );
+    console.log("indicators", indicators);
+  }, 6000);
+
+  page.on("close", () => {
+    clearInterval(pageLooper);
+  });
+
+  //await browser.close();
+})();
+
+//https://www.binance.com/en/futures/BTCBUSD?utm_source=internal&utm_medium=homepage&utm_campaign=trading_dashboard
+//https://www.binance.com/en/trade/BTC_USDT?theme=dark&type=spot
---
+++ constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  t: "Time",
+  o: "Open",
+  h: "High",
+  l: "Low",
+  c: "Close",
+  change: "Change",
+  amp: "Amplitude",
+  "MA[0]Series": "MA(7)",
+  "MA[1]Series": "MA(25)",
+  "MA[2]Series": "MA(99)",
+  "VOL[0]Series": "Vol(BTC)",
+  "VOL[1]Series": "Vol(USDT)",
+};
